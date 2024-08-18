0142620dfa87d6cc23974c580acdb621be9a5a6d
ThePenitentOne07 <144657390+ThePenitentOne07@users.noreply.github.com>
2024-08-18 14:18:43 +0700

    update image

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/files', express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 app.use('/', indexRouter);
